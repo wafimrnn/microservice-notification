e7e675a34b85444b5e8e60e9f3d2e4557a759835
wafi <165463465+wafimrnn@users.noreply.github.com>
2025-01-24 21:51:51 +0800

    Update ProductRepository.java

--- src/main/java/com/pos/stock_management_service/ProductRepository.java
+++ src/main/java/com/pos/stock_management_service/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.pos.stock_management_service;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    List<Product> findByQuantityStockLessThanEqualAndRestockLevelGreaterThanEqual(int quantityStock, int restockLevel);
+
+    // Custom query to compare QUANTITY_STOCK and RESTOCK_LEVEL
+    @Query("SELECT p FROM Product p WHERE p.quantityStock <= p.restockLevel")
+    List<Product> findProductsToRestock();
 }
