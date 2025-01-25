887ab56ef91c93c3ef9ad49df95a543e676bc815
wafi <165463465+wafimrnn@users.noreply.github.com>
2025-01-26 02:08:04 +0800

    Update NotificationController.java

--- src/main/java/com/pos/stock_management_service/NotificationController.java
+++ src/main/java/com/pos/stock_management_service/NotificationController.java
@@ -3,6 +3,8 @@ package com.pos.stock_management_service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import java.util.List;
 
